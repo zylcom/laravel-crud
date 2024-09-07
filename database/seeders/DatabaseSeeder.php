e7690599fecd3dfa5f45a54089825817f60e3de5
Sabilillah <sabilillah272@gmail.com>
2024-09-07 18:35:29 +0700

    Add product factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,6 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        Product::factory()->count(10)->create();
 
         User::factory()->create([
             'name' => 'Test User',
